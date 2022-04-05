2cb49b219a23235577cbf07c9846ed4ff4330e18
ahyeonjoung <ribbon0529@naver.com>
2022-04-05 16:48:32 +0900

    [ADD] Block

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,30 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-class Human {
-    constructor(name, age, gender) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
+const CryptoJS = require("crypto-js");
+class Block {
+    constructor(index, hash, previousHash, data, timestamp) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
     }
 }
-const ahyeon = new Human("ahyeon", 25, "female");
-const sayHi = (person) => {
-    return `Hello ${person.name} ${person.age} ${person.gender}`;
+//static: 생성하지 않아도 쓸 수 있음 
+Block.calculateBlockHash = (index, previousHash, timestamp, data) => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+const genesisBlock = new Block(0, "202020202020202", "", "Hello", 123456);
+let blockchain = [genesisBlock];
+const getBlockChain = () => blockchain;
+const getLatestBlock = () => blockchain[blockchain.length - 1];
+const getNewTimeStamp = () => Math.round(new Date().getTime() / 1000);
+const createNewBlock = (data) => {
+    const previousBlock = getLatestBlock();
+    const newIndex = previousBlock.index + 1;
+    const newTimestamp = getNewTimeStamp();
+    const newHash = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimestamp, data);
+    const newBlock = new Block(newIndex, newHash, previousBlock.hash, data, newTimestamp);
+    return newBlock;
 };
-console.log(sayHi(ahyeon));
+console.log(blockchain);
+console.log(createNewBlock("hello"), createNewBlock("bye-bye"));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,42 @@
-class Human{
-    public name: string;
-    public age: number;
-    public gender: string;
-    constructor(name:string, age:number,gender:string){
-        this.name=name;
-        this.age=age;
-        this.gender=gender;
+import * as CryptoJS from "crypto-js";
+import { decodedTextSpanIntersectsWith } from "typescript";
+class Block{
+    public index:number;
+    public hash:string;
+    public previousHash:string;
+    public data: string;
+    public timestamp: number;
+
+    //static: 생성하지 않아도 쓸 수 있음 
+    static calculateBlockHash=(index:number,previousHash:string, timestamp:number,data:string):string=>
+    CryptoJS.SHA256(index+previousHash+timestamp+data).toString();
+
+    constructor(index:number,hash:string,previousHash:string,data:string,timestamp:number){
+        this.index=index;
+        this.hash=hash;
+        this.previousHash=previousHash;
+        this.data=data;
+        this.timestamp=timestamp;
     }
 }
 
-const ahyeon=new Human("ahyeon",25,"female");
-const sayHi= (person:Human): String =>{
-    return `Hello ${person.name} ${person.age} ${person.gender}` ;
+
+const genesisBlock:Block =new Block(0,"202020202020202","","Hello",123456);
+let blockchain :Block[] =[genesisBlock];
+
+const getBlockChain =() :Block[]=>blockchain;
+const getLatestBlock=():Block =>blockchain[blockchain.length-1];
+const getNewTimeStamp=():number =>Math.round(new Date().getTime()/1000);
+const createNewBlock=(data:string):Block =>{
+    const previousBlock:Block =getLatestBlock();
+    const newIndex:number =previousBlock.index+1;
+    const newTimestamp:number=getNewTimeStamp();
+    const newHash:string =Block.calculateBlockHash(newIndex,previousBlock.hash,newTimestamp,data);
+    const newBlock:Block=new Block(newIndex,newHash,previousBlock.hash,data,newTimestamp);
+    return newBlock;
 }
 
-console.log(sayHi(ahyeon))
+console.log(blockchain);
+console.log(createNewBlock("hello"),createNewBlock("bye-bye"))
 
 export {};
